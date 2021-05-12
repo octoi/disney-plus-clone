6af50615878d8aa817db05886a29fd9efc03ef2f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-12 10:26:20 +0530

    navigation menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,30 @@ export default function Header() {
         <Nav>
             <Logo src="/images/logo.svg" />
             <NavMenu>
-
+                <a>
+                    <img src="/images/home-icon.svg" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" />
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg" />
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" />
+                    <span>SERIES</span>
+                </a>
             </NavMenu>
         </Nav>
     )
@@ -15,10 +38,29 @@ export default function Header() {
 const Nav = styled.nav`
     height: 70px;
     background-color: #090B13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
 `
+
 const Logo = styled.img`
     width: 80px;
 `
-const NavMenu = styled.div`
 
+const NavMenu = styled.div`
+    display: flex;
+    a { 
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
 `
