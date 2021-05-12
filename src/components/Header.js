58768d1b162390989c69aed10d65dea056bba664
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-12 10:44:30 +0530

    completed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,7 @@ export default function Header() {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+            <UserImg src="https://avatars.githubusercontent.com/u/82007161?s=200&v=4" />
         </Nav>
     )
 }
@@ -49,10 +50,14 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
     display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
     a { 
         display: flex;
         align-items: center;
         padding: 0 12px;
+        cursor: pointer;
 
         img {
             height: 20px;
@@ -61,6 +66,35 @@ const NavMenu = styled.div`
         span {
             font-size: 13px;
             letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover{
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
         }
     }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
