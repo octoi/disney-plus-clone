26184596a273ca629645e275b6aed77f3b379ca4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-13 20:01:12 +0530

    sample carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
+import ImgSlider from './ImgSlider';
 
 export default function Home() {
     return (
         <Container>
-            Home
+            <ImgSlider />
         </Container>
     )
 }
--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function ImgSlider() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoPlay: true
+    }
+
     return (
-        <div>
-            Image Slider
-        </div>
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" />
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" />
+            </Wrap>
+        </Carousel>
     )
 }
+
+const Carousel = styled(Slider)`
+    margin-top: 20px;
+`
+
+const Wrap = styled.div``
