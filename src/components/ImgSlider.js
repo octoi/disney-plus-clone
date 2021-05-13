6343bf1928cbc2812349278967da0e58ef8b7f60
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-13 20:12:52 +0530

    styles

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -20,7 +20,7 @@ export default function ImgSlider() {
                 <img src="/images/slider-badging.jpg" />
             </Wrap>
             <Wrap>
-                <img src="/images/slider-badging.jpg" />
+                <img src="/images/slider-badag.jpg" />
             </Wrap>
         </Carousel>
     )
@@ -28,6 +28,33 @@ export default function ImgSlider() {
 
 const Carousel = styled(Slider)`
     margin-top: 20px;
+
+    ul li button {
+        &:before{
+            font-size: 10px;
+            color: rgb(150, 158, 171);
+        }
+    }
+
+    li.slick-active button::before {
+        color: white;
+    }
+
+    .slick-list {
+        overflow: visible;
+    }
+
+    button {
+        z-index: 1;
+    }
 `
 
-const Wrap = styled.div``
+const Wrap = styled.div`
+    img {
+        border: 2px solid transparent;
+        width: 100%;
+        height: 100%;
+        border-radius: 4px;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    }
+`
