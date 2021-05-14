ba6a9fc22c33907b3ab0ca9f9bf3cb0eec86c619
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-14 10:04:03 +0530

    fixed typo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Login() {
     return (
-        <div>
+        <Container>
 
-        </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    position: relative;
+
+    &:before {
+
+    }
+`
