15673ab4f3c09f8b25042a75cfba959c597ce533
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-14 08:49:32 +0530

    basic styles for details page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Detail() {
     return (
-        <div>
+        <Container>
             Detail
-        </div>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+`;
