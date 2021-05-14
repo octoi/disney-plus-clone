0d76ba5dbb475ee326bf95f0e54412d0786973c2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-14 08:46:48 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/detail" exact>
+                        <Detail />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
