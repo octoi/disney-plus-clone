dbd1ff4c4f5440b39db0cbd318cbd656db87141d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-14 09:11:50 +0530

    added bg image

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,12 +4,35 @@ import styled from 'styled-components';
 export default function Detail() {
     return (
         <Container>
-            Detail
+            <Bg />
         </Container>
     )
 }
 
+const Bg = ({ image }) => (
+    <Background>
+        <img src={image ? image : "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/B409C2A425D58C32D822EB633C7CAE3DC910DC2FC62D2B1807A0BB092C531E9A/scale?width=1440&aspectRatio=1.78&format=jpeg"} />
+    </Background>
+);
+
 const Container = styled.div`
     min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
+    position: relative;
 `;
+
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    opacity: 0.8;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
