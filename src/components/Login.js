ec4520459e4747e68f299e0d38290054937ced95
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-14 10:06:03 +0530

    login page bg

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,17 +4,23 @@ import styled from 'styled-components';
 export default function Login() {
     return (
         <Container>
-
+            Login
         </Container>
     )
 }
 
 const Container = styled.div`
     height: calc(100vh - 70px);
-    padding: 0 calc(3.5vw + 5px);
+    /* padding: 0 calc(3.5vw + 5px); */
     position: relative;
 
     &:before {
-
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        content: "";
+        background-image: url("/images/login-background.jpg");
     }
 `
