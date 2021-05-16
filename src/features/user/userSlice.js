48e02a502f776c35ee1ff9c44abe3155ca6a7a4e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-16 07:10:49 +0530

    user slice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    name: "",
+    email: "",
+    photo: ""
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUserLogin: (state, action) => {
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+            state.photo = action.payload.photo;
+        },
+        setSignOut: (state) => {
+            state.name = null;
+            state.email = null;
+            state.photo = null;
+        }
+    }
+});
