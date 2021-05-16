a483e4d2162efc6574922dde5ab467ad8529ce99
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-16 07:13:06 +0530

    export stuffs

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -22,3 +22,11 @@ const userSlice = createSlice({
         }
     }
 });
+
+export const { setUserLogin, setSignOut } = userSlice.actions;
+
+export const selectUserName = (state) => state.user.name;
+export const selectUserEmail = (state) => state.user.email;
+export const selectUserPhoto = (state) => state.user.photo;
+
+export default userSlice.reducer;
